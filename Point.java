8eefdee43678dd3079f9fe956f1b836c4d889193
Xiao Luo <xiao2017luo@gmail.com>
2017-08-31 22:09:46 -0700

    format

--- Point.java
+++ Point.java
@@ -4,46 +4,24 @@ import edu.princeton.cs.algs4.StdDraw;
 public class Point implements Comparable<Point> {
     private final int x;     // x-coordinate of this point
     private final int y;     // y-coordinate of this point
-
-    /**
-     * Initializes a new point.
-     *
-     * @param  x the <em>x</em>-coordinate of the point
-     * @param  y the <em>y</em>-coordinate of the point
-     */
+	
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+        // Initializes a new point
     }
 
-    /**
-     * Draws this point to standard draw.
-     */
     public void draw() {
         StdDraw.point(x, y);
+        // Draws this point to standard draw
     }
 
-    /**
-     * Draws the line segment between this point and the specified point
-     * to standard draw.
-     *
-     * @param that the other point
-     */
     public void drawTo(Point that) {
         StdDraw.line(this.x, this.y, that.x, that.y);
+        // Draws the line segment between this point and the specified point to standard draw
     }
     
-    /**
-     * Returns the slope between this point and the specified point.
-     * Formally, if the two points are (x0, y0) and (x1, y1), then the slope
-     * is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
-     * +0.0 if the line segment connecting the two points is horizontal;
-     * Double.POSITIVE_INFINITY if the line segment is vertical;
-     * and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
-     *
-     * @param  that the other point
-     * @return the slope between this point and the specified point
-     */
+    
     public double slopeTo(Point that) {
         if (this.x == that.x && this.y == that.y) {
             return Double.NEGATIVE_INFINITY;
@@ -54,35 +32,29 @@ public class Point implements Comparable<Point> {
         } else {
     	      return ((double)(that.y - this.y))/(that.x - this.x);
         }
+        /*
+        Returns the slope between this point and the specified point.
+        Formally, if the two points are (x0, y0) and (x1, y1), then the slope
+        is (y1 - y0) / (x1 - x0). For completeness, the slope is defined to be
+        +0.0 if the line segment connecting the two points is horizontal;
+        Double.POSITIVE_INFINITY if the line segment is vertical;
+        and Double.NEGATIVE_INFINITY if (x0, y0) and (x1, y1) are equal.
+        */
     }
 
-    /**
-     * Compares two points by y-coordinate, breaking ties by x-coordinate.
-     * Formally, the invoking point (x0, y0) is less than the argument point
-     * (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1.
-     *
-     * @param  that the other point
-     * @return the value <tt>0</tt> if this point is equal to the argument
-     *         point (x0 = x1 and y0 = y1);
-     *         a negative integer if this point is less than the argument
-     *         point; and a positive integer if this point is greater than the
-     *         argument point
-     */
-     
+    
     public int compareTo(Point that) {
         if (this.y == that.y) {
             return this.x - that.x;
         }
         return this.y - that.y;
+        /*
+        Compares two points by y-coordinate, breaking ties by x-coordinate.
+        Formally, the invoking point (x0, y0) is less than the argument point
+        (x1, y1) if and only if either y0 < y1 or if y0 = y1 and x0 < x1
+        */
     }
 
-    /**
-     * Compares two points by the slope they make with this point.
-     * The slope is defined as in the slopeTo() method.
-     *
-     * @return the Comparator that defines this ordering on points
-     */
-     
     public Comparator<Point> slopeOrder() {
         return new Comparator<Point>(){
             public int compare(Point p1, Point p2) {
@@ -91,30 +63,20 @@ public class Point implements Comparable<Point> {
                 } else if (p1.slopeTo(Point.this) < p2.slopeTo(Point.this)) {
                     return -1;
                 } else {
-				            return 1;
+                    return 1;
                 }
             }
         };
+        // Compares two points by the slope they make with this point; The slope is defined as in the slopeTo() method
     }
-
-    /**
-     * Returns a string representation of this point.
-     * This method is provide for debugging;
-     * your program should not rely on the format of the string representation.
-     *
-     * @return a string representation of this point
-     */
-     
+  
     public String toString() {
         return "(" + x + ", " + y + ")";
+        // Returns a string representation of this point
     }
-
-    /**
-     * Unit tests the Point data type.
-     */
-     
+    
     public static void main(String[] args) {
-        /* YOUR CODE HERE */
+        // Unit tests
     	
     }
 }
